932d6211bb99622ec6a540c8094c8e97cfc9e719
Saurabh <sauravthakulla683@gmail.com>
2026-05-06 00:36:01 +0530

    add : smooth scrolling

---
+++ client/src/hooks/useLenis.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
+export const useLenis = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      smoothTouch: true,
+      touchMultiplier: 2,
+      wheelMultiplier: 1,
+      infinite: false,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+};
